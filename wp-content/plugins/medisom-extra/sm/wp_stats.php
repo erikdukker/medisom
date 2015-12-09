47719c69e41e2f5ec7c2ad18f7e2d10d0834e310
erikdukker <erik.dukker@gmail.com>
2015-12-09 10:06:36 +0100

    initieel

--- wp-content/plugins/medisom-extra/sm/wp_stats.php
+++ wp-content/plugins/medisom-extra/sm/wp_stats.php
@@ -4,14 +4,17 @@ include 'class_sessie.php';
 if ($ses->seid == 'mad') {														// sessie id loggen
 	echo 'start tellen<br>';
 	$aant	= 0;
-	$rsan 	= $wpdb->get_results("SELECT * FROM m_an");
+	$rsan 	= $wpdb->get_results("SELECT * FROM m_an order by stamp desc ip asc");
     foreach ($rsan as $rwan) {
-		if (isset($iptb[$rwan->ip])) { $iptb[$rwan->ip]++;} else { $iptb[$rwan->ip] = 1;} // aantal ip's + aantal sommen per ip
+		$key	= substr($rwan->stamp,0,7).$rwan->ip;
+		if (isset($tb[$key])) { $tb[$key]++;} else { $tb[$key] = 1;} // aantal ip's + aantal sommen per ip
 		$aant++;
 	}
 	echo '<table>';
-	foreach ($iptb as $ip => $ipan) {
-		echo '<tr><td>'.$ip.'</td><td> '.$ipan.'</td><td><a href="http://www.lookip.net/ip/'.$ip.'">waar</a></td></tr>';
+	foreach ($tb as $key => $an) {
+		$mnd	= substr($key,0,7);
+		$ip		= substr($key,7);
+		echo '<tr><td>'.$mnd.'</td><td>'.$ip.'</td><td> '.$an.'</td><td><a href="http://www.lookip.net/ip/'.$ip.'">waar</a></td></tr>';
 	}
 	echo '</table>';
 	echo 'aantal: '.$aant.'<br>';
